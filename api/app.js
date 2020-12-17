65de902f9243c0081223d8aeb46dbd1d3e5d157e
karmasakshi <karmasakshi@users.noreply.github.com>
2020-12-18 01:24:28 +0400

    feat: add cors

--- api/app.js
+++ api/app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var configRouter = require('./routes/index');
+var cors = require('cors')
 
 var app = express();
 
+app.use(cors())
 app.use(express.json());
 
 app.use('/', configRouter);
