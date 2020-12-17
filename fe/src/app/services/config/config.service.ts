b2f9cd933de57a52d257e544620e6e584b80dfe6
karmasakshi <karmasakshi@users.noreply.github.com>
2020-12-18 01:24:43 +0400

    feat: add config service

---
+++ fe/src/app/services/config/config.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ConfigOptions } from 'src/app/interfaces/config-options';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  getConfigOptions(): Observable<ConfigOptions> {
+
+    return this.httpClient.get<ConfigOptions>('http://localhost:3000');
+
+  }
+
+}
