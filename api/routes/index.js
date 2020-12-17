9bc791ad55311061bae7ce6f2166a2e39dc71599
karmasakshi <karmasakshi@users.noreply.github.com>
2020-12-17 23:56:14 +0400

    feat: add config api

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.json({
+    ctaButtonColors: ['warn', 'primary', 'accent'],
+    ctaButtonTexts: ['Let\'s Go!', 'Sign Up Now!', 'Start!']
+  });
 });
 
 module.exports = router;
