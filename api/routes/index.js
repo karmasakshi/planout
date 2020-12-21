2188f6c25ec5239ad3be324889a1dd1c26493dea
karmasakshi <karmasakshi@users.noreply.github.com>
2020-12-21 11:08:17 +0400

    feat: add form api

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const configuration = {
+  ctaButtonColors: ['warn', 'primary', 'accent'],
+  ctaButtonTexts: ['Let\'s Go!', 'Sign Up Now!', 'Start!']
+};
+
 router.get('/', function (req, res, next) {
-  res.json({
-    ctaButtonColors: ['warn', 'primary', 'accent'],
-    ctaButtonTexts: ['Let\'s Go!', 'Sign Up Now!', 'Start!']
-  });
+  res.json(configuration);
 });
+
+router.post('/', function (req, res, next) {
+  configuration = req.body;
+})
 
 module.exports = router;
